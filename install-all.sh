062ebf4002eb266ed7c5b47666945e047a07ba74
Felipe Monteiro <filtrodeoleo@gmail.com>
2020-10-01 14:55:52 +0100

    Install All

---
+++ install-all.sh
@@ -0,0 +1,9 @@
+./sublime-install.sh
+
+./ngrok-install.sh
+
+./ruby-install.sh
+
+./python-install.sh
+
+./torghost-install.sh
--- torghost-install.sh
+++ torghost-install.sh
@@ -4,8 +4,6 @@ cd torghost
 
 chmod +x build.sh
 
-./phyton-install.sh
-
 ./build.sh
 
 python3 torghost.py --start
